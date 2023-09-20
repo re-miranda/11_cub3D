3019ac9ebc1e653cc04ad4772ad8c1541039acf5
Gabriel Souza <email@gawbsouza.dev.br>
2023-09-20 16:57:00 -0300

    feat: add dependencies to cub3d.h

--- include/cub3d.h
+++ include/cub3d.h
@@ -3,19 +3,23 @@
 /*                                                        :::      ::::::::   */
 /*   cub3d.h                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: rmiranda <rmiranda@student.42sp.org.br>    +#+  +:+       +#+        */
+/*   By: gasouza <gasouza@student.42sp.org.br>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/06 14:59:06 by rmiranda          #+#    #+#             */
-/*   Updated: 2023/09/13 11:39:21 by rmiranda         ###   ########.fr       */
+/*   Updated: 2023/09/20 15:33:09 by gasouza          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef CUB3D_H
 # define CUB3D_H
 
-# include "define.h"
 # include "libft_gnl_printf.h"
 # include <stdio.h>
+# include <math.h>
+# include "mlx.h"
+# include "define.h"
+# include "helpers.h"
+# include "engine.h"
 
 int	parse(t_map_info *info_ptr, char *map_path);
 
