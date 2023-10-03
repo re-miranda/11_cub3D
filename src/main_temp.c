eecf26d9176a111d59028304997c560858369f14
remiranda <pesca_botoes.0t@icloud.com>
2023-10-03 16:20:36 -0300

    fix: remove unused function

--- src/main_temp.c
+++ src/main_temp.c
@@ -6,7 +6,7 @@
 /*   By: rmiranda <rmiranda@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/20 09:01:33 by gasouza           #+#    #+#             */
-/*   Updated: 2023/10/03 16:16:35 by rmiranda         ###   ########.fr       */
+/*   Updated: 2023/10/03 16:20:07 by rmiranda         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 #include "parse.h"
 #include <string.h>
 
-static void temp_config_create(t_game *game);
+// static void temp_config_create(t_game *game);
 static void temp_config_destroy(t_game *game);
 
 static void	print_map(t_map_info info)
@@ -91,62 +91,62 @@ int	main(int argc, char *argv[])
 	return (0);
 }
 
-static void temp_config_create(t_game *game)
-{
-	game->info.m_width = 16;
-	game->info.m_height = 16;
+// static void temp_config_create(t_game *game)
+// {
+// 	game->info.m_width = 16;
+// 	game->info.m_height = 16;
 
-	game->info.path_no = strdup("./img/red.xpm");
-	game->info.path_so = strdup("./img/yellow.xpm");
-	game->info.path_we = strdup("./img/pink.xpm");
-	game->info.path_ea = strdup("./img/blue.xpm");
+// 	game->info.path_no = strdup("./img/red.xpm");
+// 	game->info.path_so = strdup("./img/yellow.xpm");
+// 	game->info.path_we = strdup("./img/pink.xpm");
+// 	game->info.path_ea = strdup("./img/blue.xpm");
 	
-	char map[16][16] = {
-		{2,2,1,1,1,1,1,1,1,1,1,1,1,1,1,1},
-		{2,2,1,0,0,0,0,0,0,0,0,0,0,0,0,1},
-		{1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,1},
-		{1,0,'N',0,0,0,0,0,0,0,0,0,0,0,0,1},
-		{1,0,0,0,1,1,1,0,0,1,1,1,0,0,0,1},
-		{1,0,0,0,1,0,0,0,0,0,0,1,0,0,0,1},
-		{1,0,0,0,1,0,0,0,0,0,0,1,0,0,0,1},
-		{1,0,0,0,1,0,0,0,0,0,0,1,0,0,0,1},
-		{1,0,0,0,1,1,1,0,0,1,1,1,0,0,0,1},
-		{1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
-		{1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
-		{1,0,0,0,1,1,1,1,1,1,1,1,0,0,0,1},
-		{1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
-		{1,0,0,0,1,1,1,1,1,1,1,1,0,0,0,1},
-		{1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
-		{1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1}
-	};
+// 	char map[16][16] = {
+// 		{2,2,1,1,1,1,1,1,1,1,1,1,1,1,1,1},
+// 		{2,2,1,0,0,0,0,0,0,0,0,0,0,0,0,1},
+// 		{1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,1},
+// 		{1,0,'N',0,0,0,0,0,0,0,0,0,0,0,0,1},
+// 		{1,0,0,0,1,1,1,0,0,1,1,1,0,0,0,1},
+// 		{1,0,0,0,1,0,0,0,0,0,0,1,0,0,0,1},
+// 		{1,0,0,0,1,0,0,0,0,0,0,1,0,0,0,1},
+// 		{1,0,0,0,1,0,0,0,0,0,0,1,0,0,0,1},
+// 		{1,0,0,0,1,1,1,0,0,1,1,1,0,0,0,1},
+// 		{1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+// 		{1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+// 		{1,0,0,0,1,1,1,1,1,1,1,1,0,0,0,1},
+// 		{1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+// 		{1,0,0,0,1,1,1,1,1,1,1,1,0,0,0,1},
+// 		{1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+// 		{1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1}
+// 	};
 	
-	game->info.map = malloc(game->info.m_height * sizeof(char *));
-	game->info.color_c = 0x00000540;	// Blue
-	game->info.color_f = 0x003d1100;	// Brown
+// 	game->info.map = malloc(game->info.m_height * sizeof(char *));
+// 	game->info.color_c = 0x00000540;	// Blue
+// 	game->info.color_f = 0x003d1100;	// Brown
 
-	for (int x = 0; x < game->info.m_height; x++) {
-		game->info.map[x] = malloc(game->info.m_width);
-		for (int y = 0; y < game->info.m_width; y++) {
+// 	for (int x = 0; x < game->info.m_height; x++) {
+// 		game->info.map[x] = malloc(game->info.m_width);
+// 		for (int y = 0; y < game->info.m_width; y++) {
 
-			char element = map[y][x];
+// 			char element = map[y][x];
 			
-			if (element != FLOOR && element != WALL && element != EMPTY) // Player positon
-			{
-				game->info.pos.y = y;
-				game->info.pos.x = x;
-				game->info.orient = element;
-				element = FLOOR;
-			}
+// 			if (element != FLOOR && element != WALL && element != EMPTY) // Player positon
+// 			{
+// 				game->info.pos.y = y;
+// 				game->info.pos.x = x;
+// 				game->info.orient = element;
+// 				element = FLOOR;
+// 			}
 			
-			game->info.map[x][y] = element;
-		}
-	}
+// 			game->info.map[x][y] = element;
+// 		}
+// 	}
 
-	game->info.m_width = game->info.m_width;
-	game->info.m_height = game->info.m_height;
+// 	game->info.m_width = game->info.m_width;
+// 	game->info.m_height = game->info.m_height;
 
-	printf("Temporary Configuration: created!\n");
-}
+// 	printf("Temporary Configuration: created!\n");
+// }
 
 static void temp_config_destroy(t_game *game)
 {
